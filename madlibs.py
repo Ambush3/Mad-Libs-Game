cf6361beea580dedd71b6cc7cc9f028e00efd065
Ambush3 <81124911+Ambush3@users.noreply.github.com>
2021-03-22 17:48:44 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+# input name here
+name = input("What is your name?")
+
+#input from user
+exclamation = input("Enter an exclamation word and hit enter: ")
+adverb = input("Enter an adverb word and hit enter: ")
+noun = input("Enter a noun word and hit enter: ")
+adjective = input("Enter an adjective word and hit enter: ")
+verb = input("Enter a verb word and hit enter: ")
+
+# the story
+story = f" Put your hands up, said the {noun}. Youre under arrest for {verb} the {noun} at the {noun}. {exclamation} I thought it was Bob. His hands were {verb} like {noun} on a {noun}."
+
+
+
+print(story)
